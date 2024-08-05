dafc6b613d66b899c77e435e84f50f5aa5042a9d
KiwiScreams <screamskiwi3@gmail.com>
2024-08-05 17:02:52 +0400

    responsive design

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ import pinterest from "./assets/images/icon-pinterest.svg";
 import twitter from "./assets/images/icon-twitter.svg";
 
 const Nav = ({handleToggleVisibility, isVisible}) => {
-  
+   
   return (
     <>
       <div className={`nav-container ${isVisible ? "visible" : "hidden"}`}>
@@ -15,6 +15,17 @@ const Nav = ({handleToggleVisibility, isVisible}) => {
             <img src={pinterest} alt="" />
             <img src={twitter} alt="" />
           </div>
+          <button
+                onClick={handleToggleVisibility}
+                className={isVisible ? "active" : ""}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
+                  <path
+                    fill="#6E8098"
+                    d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+                  />
+                </svg>
+              </button>
           <div className="triangle"></div>
         </div>
       </div>
